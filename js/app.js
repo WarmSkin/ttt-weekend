d452a24ab8b9eb8dac45b5799051ebcb2040f6b2
Pin Chen <pamyc1876@gmail.com>
2022-12-08 17:07:32 -0500

    Start the ttt

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const gameStack = {
+    sq0: undefined,
+    sq1: undefined,
+    sq2: undefined,
+    sq3: undefined,
+    sq4: undefined,
+    sq5: undefined,
+    sq6: undefined,
+    sq7: undefined,
+    sq8: undefined,
+}
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let player1Move = true;
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -11,8 +21,11 @@
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+document.querySelector(".board").addEventListener('click', playerMove);
 
 
 /*-------------------------------- Functions --------------------------------*/
 
+function playerMove(e){
+    e.target.textContent = 
+}
